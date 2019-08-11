d2e8f9df9861ddae10071461689ed90dee4d16e3
sjohn164 <sheena9982@gmail.com>
2019-08-11 11:49:24 -0400

    first commit

---
+++ helloWorld.js
@@ -0,0 +1,18 @@
+
+
+function printMyMessage(shouldWeDoStuff){
+    if (!shouldWeDoStuff){
+        for(let i = 0; i<=2; i+=1){
+            console.log('hello world');
+            console.log('hi Sheena');
+            console.log('hello world');  
+        }     
+    } else if (shouldWeDoStuff ==''){
+        console.log('kiaIsLovely');
+    }
+    
+
+    console.log('eat dirt');   
+}
+printMyMessage(false);
+//printMyMessage(false);
